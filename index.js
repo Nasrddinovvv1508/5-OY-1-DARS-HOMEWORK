d75226e02edf5b50556adcff5b66cc4575d07137
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-01-11 22:00:50 +0500

    All Done

---
+++ index.js
@@ -0,0 +1,50 @@
+        // 1-MASALA
+// let arr = [1, 2, `salom`, true, 102, undefined];
+// let sum = 0;
+
+// function arr_check(arg) {
+//     arr.forEach((arg) => {
+        
+//         if (typeof arg == `number`) {
+//             sum += arg;
+//         }
+//     })
+// };
+
+// arr_check(arr);
+// console.log(sum);
+
+
+
+
+        // 2-MASALA
+// let yozuv = "salom bolalar yaxshmiszlar";
+
+// function kattaBirinchiHarf(arg) {
+//     let sozlar = arg.split(" ");
+//     for (let i = 0; i < sozlar.length; i++) {
+//         let birinchi_harf_katta = sozlar[i].charAt(0).toUpperCase() + sozlar[i].slice(1);
+//         sozlar[i] = birinchi_harf_katta;
+//         }
+//             return sozlar.join(" ");
+//         }
+        
+// console.log(kattaBirinchiHarf(yozuv));
+
+
+
+        // 3-MASALA
+// let str = `alla`;
+
+// let str2 = Array.from(str).reverse().join('');
+
+// if (str == str2) {
+//     console.log(`Bu so'z palindrom`);
+// } else {
+//     console.log(`Bu so'z palindrom emas`);
+// };
+
+
+
+
+        // 4-MASALA
